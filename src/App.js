b7620e7dd88a0fd2aba837ac2cf50cec09ef79cc
CatinPajamas <catinpajamas1997@gmail.com>
2025-01-01 23:31:18 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+
+import { useEffect } from 'react';
+import './App.css';
+import CartContainer from './components/CartContainer';
+import Navbar from './components/Navbar';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals } from './features/cart/CartSlice';
+import Modal from './components/Modal';
+
+
+function App() {
+  const dispatch = useDispatch();
+  const {cartItems} = useSelector((state) => state.cart);
+  const {isOpen} = useSelector((state) => state.modal);
+
+  useEffect(() =>{
+    dispatch(calculateTotals());
+  },[cartItems])
+
+  
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;

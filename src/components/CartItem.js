433c516944db8f3626250d82c9b708c9bb654569
CatinPajamas <catinpajamas1997@gmail.com>
2025-01-08 23:03:45 +0900

    Edit the alternative word when item images crush.

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -10,7 +10,7 @@ const CartItem = ({ id,img,title,price,amount }) => {
     return (
 
     <article className="cart-item">
-        <img src={img} alt="/" />
+        <img src={img} alt="商品の画像" />
         <div>
             <h4>{title}</h4>
             <h4 className="item-price">{price}円</h4>
